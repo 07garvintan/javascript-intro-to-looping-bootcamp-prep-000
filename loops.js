ee847fcde1efc32c91fb64f051f3d4bd2f766b57
garvin tan <garvintan07@gmail.com>
2019-06-18 23:40:47 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,3 +16,13 @@ function whileLoop(n) {
   }
   return "done"
 }
+
+function doWhileLoop(n) {
+  function incrementVariable() {
+    i +=1;
+    return i;
+  }
+  do {
+    console.log("I run once regardless.");
+    } while (incrementVariable() <n)
+}

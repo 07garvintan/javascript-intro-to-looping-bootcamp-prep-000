e09bf8a1620795d3aec65a076a4a40cd9b27d4de
garvin tan <garvintan07@gmail.com>
2019-06-18 23:24:47 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
-  for ( var i = 0; i < 26; i++) {
-    array.push(`I am ${i} strange loops.`)
+  for ( var i = 0; i < 25; i++) {
+    array.push(`I am ${i} strange loops.`);
   }
 }
 
